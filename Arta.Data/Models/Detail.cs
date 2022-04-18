7b0ff40437636a08418571f4bfde79a02d2c670f
MahdiPezeshkian <75138401+MahdiPezeshkian@users.noreply.github.com>
2022-04-18 08:00:57 -0700

    Models were added

---
+++ Arta.Data/Models/Detail.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Arta.Data.Models
+{
+    public class Detail
+    {
+        [Key]
+        public int DetailId { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string Description { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string Name { get; set; }
+    }
+}
---
+++ Arta.Data/Models/Main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Arta.Data.Models
+{
+    public class Main
+    {
+        [Key]
+        public int MainId { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string Name { get; set; }
+    }
+}
